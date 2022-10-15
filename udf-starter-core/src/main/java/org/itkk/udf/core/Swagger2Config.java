b6673faf6e5a55b79fd04ce06653bac4a3d43dd1
Doomjunkys <bicun0408@yeah.net>
2022-10-16 07:12:57 +0800

    更新swagger的包扫描方式

--- udf-starter-core/src/main/java/org/itkk/udf/core/Swagger2Config.java
+++ udf-starter-core/src/main/java/org/itkk/udf/core/Swagger2Config.java
@@ -9,9 +9,9 @@ package org.itkk.udf.core;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -66,8 +66,9 @@ public class Swagger2Config {
      */
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("org.itkk")).paths(PathSelectors.any())
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
                 .build();
     }
 
