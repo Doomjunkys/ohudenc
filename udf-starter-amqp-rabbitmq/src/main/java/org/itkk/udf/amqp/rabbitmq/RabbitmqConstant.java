b1cb1ff7be479656e28ad216dc91bfd3746e8f6a
Doomjunkys <bicun0408@yeah.net>
2022-10-29 12:08:38 +0800

    mq常量

---
+++ udf-starter-amqp-rabbitmq/src/main/java/org/itkk/udf/amqp/rabbitmq/RabbitmqConstant.java
@@ -0,0 +1,32 @@
+/**
+ * BaseMqConstant.java
+ * Created at 2016-12-05
+ * Created by Administrator
+ * Copyright (C) 2016 egridcloud.com, All rights reserved.
+ */
+package org.itkk.udf.amqp.rabbitmq;
+
+/**
+ * 描述 : BaseMqConstant
+ *
+ * @author Administrator
+ */
+public class RabbitmqConstant {
+
+    /**
+     * 描述 : X_DEAD_LETTER_EXCHANGE
+     */
+    public static final String X_DEAD_LETTER_EXCHANGE = "x-dead-letter-exchange";
+
+    /**
+     * 描述 : X_DEAD_LETTER_ROUTING_KEY
+     */
+    public static final String X_DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
+
+    /**
+     * 描述 : 私有化构造函数
+     */
+    private RabbitmqConstant() {
+    }
+
+}
