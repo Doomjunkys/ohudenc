009f53f3b0adf57861d940ba59ef64b10665e963
Doomjunkys <bicun0408@yeah.net>
2024-02-01 19:58:52 +0800

    更新

--- admin/config/index.js
+++ admin/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/admin',
     proxyTable: {},
 
     // Various Dev Server settings
--- admin/config/test.env.js
+++ admin/config/test.env.js
@@ -5,5 +5,5 @@ const devEnv = require('./dev.env')
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
   VUE_APP_TITLE: '"ITKK控制台"',
-  VUE_APP_BASE_URL:'"http://127.0.0.1:9000/"'
+  VUE_APP_BASE_URL:'"/"'
 })
