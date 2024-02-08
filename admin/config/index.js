13b1cbd69fd2bf611ff292434dd60b16f6895bd6
Doomjunkys <bicun0408@yeah.net>
2024-02-09 00:01:22 +0800

    更新

--- admin/config/index.js
+++ admin/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/admin',
+    assetsPublicPath: '/admin/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/admin',
+    assetsPublicPath: '/admin/',
 
     /**
      * Source Maps
