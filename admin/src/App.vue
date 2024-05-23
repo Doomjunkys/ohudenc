99942e62953b328448d9f69405969ec043aaf9e1
Doomjunkys <bicun0408@yeah.net>
2024-05-24 00:04:13 +0800

    更新

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -236,35 +236,25 @@
       {
         var overscroll = function (el) {
           el.addEventListener('touchstart', function () {
-            var top = el.scrollTop
-              , totalScroll = el.scrollHeight
-              , currentScroll = top + el.offsetHeight
-            //If we're at the top or the bottom of the containers
-            //scroll, push up or down one pixel.
-            //
-            //this prevents the scroll from "passing through" to
-            //the body.
+            var top = el.scrollTop, totalScroll = el.scrollHeight, currentScroll = top + el.offsetHeight
             if (top === 0) {
               el.scrollTop = 1
             } else if (currentScroll === totalScroll) {
               el.scrollTop = top - 1
             }
-          })
+          });
           el.addEventListener('touchmove', function (evt) {
-            //if the content is actually scrollable, i.e. the content is long enough
-            //that scrolling can occur
-            if (el.offsetHeight < el.scrollHeight)
+            if (el.offsetHeight < el.scrollHeight) {
               evt._isScroller = true
-          })
-        }
+            }
+          });
+        };
         overscroll(document.querySelector('.outer'));
         document.body.addEventListener('touchmove', function (evt) {
-          //In this case, the default behavior is scrolling the body, which
-          //would result in an overflow.  Since we don't want that, we preventDefault.
           if (!evt._isScroller) {
             evt.preventDefault()
           }
-        })
+        });
       }
       //初始化
       this.init();
