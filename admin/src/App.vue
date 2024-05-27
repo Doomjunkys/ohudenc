cae28b7fb7bbdb0feb05780e840409ba2a864315
Doomjunkys <bicun0408@yeah.net>
2024-05-28 07:43:23 +0800

    更新

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="app outer" :style="appStyle"
+  <div id="app" class="app" :style="appStyle"
        @touchstart="touchStart"
        @touchmove="touchMove"
        @touchend="touchEnd">
@@ -233,29 +233,6 @@
       }
     },
     mounted() {
-      {
-        var overscroll = function (el) {
-          el.addEventListener('touchstart', function () {
-            var top = el.scrollTop, totalScroll = el.scrollHeight, currentScroll = top + el.offsetHeight
-            if (top === 0) {
-              el.scrollTop = 1
-            } else if (currentScroll === totalScroll) {
-              el.scrollTop = top - 1
-            }
-          });
-          el.addEventListener('touchmove', function (evt) {
-            if (el.offsetHeight < el.scrollHeight) {
-              evt._isScroller = true
-            }
-          });
-        };
-        overscroll(document.querySelector('.outer'));
-        document.body.addEventListener('touchmove', function (evt) {
-          if (!evt._isScroller) {
-            evt.preventDefault()
-          }
-        }, {passive: true});
-      }
       //初始化
       this.init();
     },
