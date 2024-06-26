b3894be698a397e1c4dc4a4a93ec344e6001e991
Doomjunkys <bicun0408@yeah.net>
2024-06-26 10:10:13 +0800

    提交

--- udf-app/main.js
+++ udf-app/main.js
@@ -5,6 +5,9 @@ Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+import uView from 'uview-ui';
+Vue.use(uView);
+
 const app = new Vue({
 	...App
 })
--- udf-app/pages/index/index.vue
+++ udf-app/pages/index/index.vue
@@ -1,8 +1,26 @@
 <template>
-	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+	<view>
+		<u-navbar :is-fixed="true" :is-back="false" title-color="#fff" :background="{'background-image': 'linear-gradient(45deg, rgb(28, 187, 180), rgb(141, 198, 63))'}">
+			<view class="slot-wrap">
+				<view class="search-wrap">
+					<!-- 如果使用u-search组件，必须要给v-model绑定一个变量 -->
+					<u-search height="56" :show-action="false" :action-style="{color: '#fff'}"></u-search>
+				</view>
+			</view>
+			<view class="navbar-right" slot="right">
+				<view class="message-box right-item">
+					<u-icon name="chat" size="56"></u-icon>
+					<!-- <u-badge count="18" size="mini" :offset="[-15, -15]"></u-badge> -->
+				</view>
+				<view class="dot-box right-item">
+					<u-icon name="calendar-fill" size="56"></u-icon>
+					<!-- <u-badge size="mini" :is-dot="true" :offset="[-6, -6]"></u-badge> -->
+				</view>
+			</view>
+		</u-navbar>
+		<view class="container">
+			<u-button>按钮</u-button>
+		</view>
 	</view>
 </template>
 
@@ -24,5 +42,30 @@
 		padding: 20px;
 		font-size: 14px;
 		line-height: 24px;
+	}
+</style>
+
+<style lang="scss" scoped>
+	.slot-wrap {
+		display: flex;
+		align-items: center;
+		flex: 1;
+	}
+
+	.search-wrap {
+		margin: 0 20rpx;
+		flex: 1;
+	}
+
+	.navbar-right {
+		margin-right: 10rpx;
+		display: flex;
+	}
+
+	.right-item {
+		margin: 0 10rpx;
+		position: relative;
+		color: #ffffff;
+		display: flex;
 	}
 </style>
